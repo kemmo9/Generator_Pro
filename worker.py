221315f8a319680f6987fe189751d7e9866c3841
kemmo9 <kesser59@gmail.com>
2025-06-29 23:30:57 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,11 +1,9 @@
 import os
 import redis
 from rq import Worker, Queue
-
 import tasks
 
 listen = ['default']
-
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
