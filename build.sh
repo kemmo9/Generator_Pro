c3d58021f7d726e15defdfaeab3f734a1c677dbf
kemmo9 <kesser59@gmail.com>
2025-07-02 16:30:45 +0300

    Update and rename build.sh to build.py

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# This line tells Render's build server to install the ImageMagick software
-apt-get update && apt-get install -y imagemagick
-
-# This line installs all your Python packages from requirements.txt
-pip install -r requirements.txt
